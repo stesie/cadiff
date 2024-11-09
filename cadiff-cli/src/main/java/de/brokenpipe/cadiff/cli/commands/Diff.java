7ead146444a1fd38d20bdecae192c7c56258c3b6
Stefan Siegl <stesie@brokenpipe.de>
2024-11-09 11:19:59 +0100

    allow to dump changeset

--- cadiff-cli/src/main/java/de/brokenpipe/cadiff/cli/commands/Diff.java
+++ cadiff-cli/src/main/java/de/brokenpipe/cadiff/cli/commands/Diff.java
@@ -1,12 +1,17 @@
 package de.brokenpipe.cadiff.cli.commands;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator;
 import de.brokenpipe.cadiff.core.diff.boundary.DiffCommand;
 import de.brokenpipe.cadiff.core.diff.entity.ChangeSet;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.Callable;
 
 @Command(name = "diff", description = "Diff two BPMN files")
@@ -15,10 +20,27 @@ public class Diff implements Callable<Integer> {
 	@Parameters(arity = "2", description = "two bpmn files to compare")
 	File[] files;
 
+	@Option(names = { "-d", "--dump-changeset" }, paramLabel = "CHANGESET", description = "the changeset dump file")
+	File dumpChangeSet;
+
 	@Override
 	public Integer call() throws Exception {
 		final ChangeSet changeSet = new DiffCommand(files[0], files[1]).execute();
+
+		if (dumpChangeSet != null) {
+			dumpChangeSet(changeSet);
+		}
+
 		return Integer.valueOf(0);
+	}
+
+	private void dumpChangeSet(final ChangeSet changeSet) {
+		try {
+			final var mapper = new ObjectMapper(new YAMLFactory().disable(YAMLGenerator.Feature.WRITE_DOC_START_MARKER));
+			mapper.writeValue(dumpChangeSet, changeSet);
+		} catch (final IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	public static void main(final String[] args) {
--- cadiff-core/src/main/java/de/brokenpipe/cadiff/core/actions/Action.java
+++ cadiff-core/src/main/java/de/brokenpipe/cadiff/core/actions/Action.java
@@ -1,4 +1,11 @@
 package de.brokenpipe.cadiff.core.actions;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({ "type", "id" })
 public interface Action {
+
+	default String getType() {
+		return this.getClass().getName();
+	}
 }

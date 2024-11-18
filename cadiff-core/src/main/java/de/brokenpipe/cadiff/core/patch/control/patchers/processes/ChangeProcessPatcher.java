2191a1d83777bc8f1e17c3e1ba8ae6dfa8f0cbad
Stefan Siegl <stesie@brokenpipe.de>
2024-11-18 22:42:51 +0100

    fix (Sub)ProcessPatcher to iterate all actions (again) + create inner context only once

--- cadiff-core/src/main/java/de/brokenpipe/cadiff/core/patch/control/patchers/processes/ChangeProcessPatcher.java
+++ cadiff-core/src/main/java/de/brokenpipe/cadiff/core/patch/control/patchers/processes/ChangeProcessPatcher.java
@@ -2,34 +2,24 @@ package de.brokenpipe.cadiff.core.patch.control.patchers.processes;
 
 import de.brokenpipe.cadiff.core.actions.Action;
 import de.brokenpipe.cadiff.core.actions.processes.ChangeProcessAction;
+import de.brokenpipe.cadiff.core.patch.control.patchers.AbstractPatcher;
 import de.brokenpipe.cadiff.core.patch.control.patchers.Patcher;
-import de.brokenpipe.cadiff.core.patch.control.patchers.exceptions.TargetElementNotFoundException;
 import de.brokenpipe.cadiff.core.patch.entity.PatcherContext;
 import lombok.RequiredArgsConstructor;
 import org.camunda.bpm.model.bpmn.instance.Process;
-import org.camunda.bpm.model.xml.instance.ModelElementInstance;
 
 @RequiredArgsConstructor
-public class ChangeProcessPatcher implements Patcher {
+public class ChangeProcessPatcher extends AbstractPatcher implements Patcher {
 
 	final ChangeProcessAction action;
 
 	@Override
 	public void accept(final PatcherContext context) {
+		final Process process = findTargetWithType(context, action.id(), Process.class);
+		final PatcherContext innerContext = context.withProcess(process);
 
 		for (final Action a : action.actions()) {
-			final ModelElementInstance target = context.getModelInstance().getModelElementById(action.id());
-
-			if (target == null) {
-				throw new TargetElementNotFoundException(action.id());
-			}
-
-			if (target instanceof final Process process) {
-				a.getPatcher().accept(context.withProcess(process));
-				return;
-			}
-
-			throw new IllegalStateException("Target element is not a Process");
+			a.getPatcher().accept(innerContext);
 
 		}
 
--- cadiff-core/src/main/java/de/brokenpipe/cadiff/core/patch/control/patchers/processes/ChangeSubProcessPatcher.java
+++ cadiff-core/src/main/java/de/brokenpipe/cadiff/core/patch/control/patchers/processes/ChangeSubProcessPatcher.java
@@ -2,34 +2,25 @@ package de.brokenpipe.cadiff.core.patch.control.patchers.processes;
 
 import de.brokenpipe.cadiff.core.actions.Action;
 import de.brokenpipe.cadiff.core.actions.processes.ChangeSubProcessAction;
+import de.brokenpipe.cadiff.core.patch.control.patchers.AbstractPatcher;
 import de.brokenpipe.cadiff.core.patch.control.patchers.Patcher;
-import de.brokenpipe.cadiff.core.patch.control.patchers.exceptions.TargetElementNotFoundException;
 import de.brokenpipe.cadiff.core.patch.entity.PatcherContext;
 import lombok.RequiredArgsConstructor;
 import org.camunda.bpm.model.bpmn.instance.SubProcess;
-import org.camunda.bpm.model.xml.instance.ModelElementInstance;
 
 @RequiredArgsConstructor
-public class ChangeSubProcessPatcher implements Patcher {
+public class ChangeSubProcessPatcher extends AbstractPatcher implements Patcher {
 
 	final ChangeSubProcessAction action;
 
 	@Override
 	public void accept(final PatcherContext context) {
 
+		final SubProcess subProcess = findTargetWithType(context, action.id(), SubProcess.class);
+		final PatcherContext innerContext = context.withProcess(subProcess);
+
 		for (final Action a : action.actions()) {
-			final ModelElementInstance target = context.getModelInstance().getModelElementById(action.id());
-
-			if (target == null) {
-				throw new TargetElementNotFoundException(action.id());
-			}
-
-			if (target instanceof final SubProcess subProcess) {
-				a.getPatcher().accept(context.withProcess(subProcess));
-				return;
-			}
-
-			throw new IllegalStateException("Target element is not a SubProcess");
+			a.getPatcher().accept(innerContext);
 		}
 
 	}

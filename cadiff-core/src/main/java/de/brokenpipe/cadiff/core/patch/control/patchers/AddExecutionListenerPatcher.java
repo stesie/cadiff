1f56f612d8902706586717dd2b465eb12364f4eb
Stefan Siegl <stesie@brokenpipe.de>
2024-11-20 21:17:47 +0100

    fix NPE / create ExtensionElements node

--- cadiff-core/src/main/java/de/brokenpipe/cadiff/core/patch/control/patchers/AddExecutionListenerPatcher.java
+++ cadiff-core/src/main/java/de/brokenpipe/cadiff/core/patch/control/patchers/AddExecutionListenerPatcher.java
@@ -4,7 +4,10 @@ import de.brokenpipe.cadiff.core.actions.AddExecutionListenerAction;
 import de.brokenpipe.cadiff.core.patch.entity.PatcherContext;
 import lombok.RequiredArgsConstructor;
 import org.camunda.bpm.model.bpmn.instance.BaseElement;
+import org.camunda.bpm.model.bpmn.instance.ExtensionElements;
 import org.camunda.bpm.model.bpmn.instance.camunda.CamundaExecutionListener;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 public class AddExecutionListenerPatcher extends AbstractPatcher implements Patcher {
@@ -15,7 +18,14 @@ public class AddExecutionListenerPatcher extends AbstractPatcher implements Patc
 	public void accept(final PatcherContext context) {
 		final var baseEl = findTargetWithType(context, action.id(), BaseElement.class);
 
-		final var ext = baseEl.getExtensionElements().addExtensionElement(CamundaExecutionListener.class);
+		final ExtensionElements extEl = Optional.ofNullable(baseEl.getExtensionElements())
+				.orElseGet(() -> {
+					final var newExtEl = context.getModelInstance().newInstance(ExtensionElements.class);
+					baseEl.setExtensionElements(newExtEl);
+					return newExtEl;
+				});
+
+		final var ext = extEl.addExtensionElement(CamundaExecutionListener.class);
 		ext.setCamundaEvent(action.camundaEvent());
 		ext.setCamundaClass(action.camundaClass());
 		ext.setCamundaDelegateExpression(action.camundaDelegateExpression());
